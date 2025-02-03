baeb4154f8daf872046bdab25b902a00a8fad217
Kiang Kuang <kiangkuang@gmail.com>
2025-02-03 19:35:48 +0800

    use useSortable for drag-and-drop functionality

--- src/views/ConfigView.vue
+++ src/views/ConfigView.vue
@@ -3,12 +3,19 @@ import { useUrlManagement } from "@/composables/useUrlManagement";
 import { RouteName } from "@/router";
 import { useItemStore } from "@/stores/itemStore";
 import { compressData, getContrastColor } from "@/utils/utils";
+import { useSortable } from "@vueuse/integrations/useSortable";
 import { storeToRefs } from "pinia";
-import draggable from "vuedraggable";
+import { useTemplateRef } from "vue";
 
 const { items, sumWeights } = storeToRefs(useItemStore());
 const { addItem, removeItem } = useItemStore();
 const { copyUrl, copyBtnText } = useUrlManagement();
+
+const el = useTemplateRef<HTMLElement>("el");
+useSortable(el, items, {
+  animation: 150,
+  handle: ".drag-handle",
+});
 </script>
 
 <template>
@@ -16,59 +23,61 @@ const { copyUrl, copyBtnText } = useUrlManagement();
     <h1>Wheel Configuration</h1>
 
     <div class="mb-4">
-      <draggable v-model="items" item-key="label" handle=".drag-handle">
-        <template #item="{ element: item, index }">
-          <div class="row g-2 pb-2 my-1 align-items-center rounded">
-            <div class="col-1">
-              <div class="drag-handle">⋮⋮</div>
-            </div>
-            <div class="col-11 col-md">
-              <input v-model.trim="item.label" class="form-control" placeholder="Label" />
-            </div>
-            <div class="col offset-1 col-md-2 offset-md-0">
+      <div ref="el">
+        <div
+          v-for="(item, index) in items"
+          :key="index"
+          class="row g-2 pb-2 my-1 align-items-center rounded"
+        >
+          <div class="col-1">
+            <div class="drag-handle">⋮⋮</div>
+          </div>
+          <div class="col-11 col-md">
+            <input v-model.trim="item.label" class="form-control" placeholder="Label" />
+          </div>
+          <div class="col offset-1 col-md-2 offset-md-0">
+            <input
+              v-model.trim="item.backgroundColor"
+              class="form-control"
+              placeholder="Color"
+              list="colorOptions"
+              :style="{
+                backgroundColor: item.backgroundColor,
+                color: getContrastColor(item.backgroundColor),
+              }"
+            />
+            <datalist id="colorOptions">
+              <option value="red"></option>
+              <option value="blue"></option>
+              <option value="green"></option>
+              <option value="yellow"></option>
+              <option value="orange"></option>
+              <option value="purple"></option>
+              <option value="pink"></option>
+              <option value="navy"></option>
+              <option value="teal"></option>
+              <option value="brown"></option>
+            </datalist>
+          </div>
+          <div class="col-5 col-md-3">
+            <div class="input-group">
               <input
-                v-model.trim="item.backgroundColor"
+                v-model.number="item.weight"
+                type="number"
                 class="form-control"
-                placeholder="Color"
-                list="colorOptions"
-                :style="{
-                  backgroundColor: item.backgroundColor,
-                  color: getContrastColor(item.backgroundColor),
-                }"
+                min="1"
+                placeholder="Weight"
               />
-              <datalist id="colorOptions">
-                <option value="red"></option>
-                <option value="blue"></option>
-                <option value="green"></option>
-                <option value="yellow"></option>
-                <option value="orange"></option>
-                <option value="purple"></option>
-                <option value="pink"></option>
-                <option value="navy"></option>
-                <option value="teal"></option>
-                <option value="brown"></option>
-              </datalist>
-            </div>
-            <div class="col-5 col-md-3">
-              <div class="input-group">
-                <input
-                  v-model.number="item.weight"
-                  type="number"
-                  class="form-control"
-                  min="1"
-                  placeholder="Weight"
-                />
-                <span class="input-group-text text-secondary">
-                  {{ Math.round((item.weight / sumWeights) * 1000) / 10 }}%
-                </span>
-              </div>
-            </div>
-            <div class="col-auto">
-              <button @click="removeItem(index)" class="btn btn-danger w-100">×</button>
+              <span class="input-group-text text-secondary">
+                {{ Math.round((item.weight / sumWeights) * 1000) / 10 }}%
+              </span>
             </div>
           </div>
-        </template>
-      </draggable>
+          <div class="col-auto">
+            <button @click="removeItem(index)" class="btn btn-danger w-100">×</button>
+          </div>
+        </div>
+      </div>
 
       <button @click="addItem()" class="btn btn-primary">Add Item</button>
     </div>
